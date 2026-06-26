b9786f05cee7ee775efa8a6ce921a5fd27b84fb0
Tharkminos <guilhermemkii@gmail.com>
2026-06-26 10:30:07 -0300

    Banaca

--- admin/routes.py
+++ admin/routes.py
@@ -1,14 +1,14 @@
 from flask import render_template
 from . import admin_bp
-
-
+from .auth import *
+from .utils import get_system_info 
 @admin_bp.route("/")
 @permission_required("admin")
 def index():
     return render_template("admin/index.html")
 
 
-from .utils import get_system_info 
+
 @admin_bp.route("/dashboard")
 @permission_required("admin")
 def dashboard():
