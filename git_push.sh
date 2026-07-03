42022060d8301623dd238bf99574aeaf7e3b0986
Tharkminos <guilhermemkii@gmail.com>
2026-07-03 09:15:24 -0300

    commit

---
+++ git_push.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+git add .
+commit='user'
+echo "Escreva seu commit"
+read commit 
+echo "Commit: $commit gerado!"
+git commit -m 'commit'
+git push
+Tharkminos
+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
+
