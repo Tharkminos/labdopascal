746027c8b9e3afdb0fbb2190540150bb8f4d4f31
Tharkminos <guilhermemkii@gmail.com>
2026-07-03 09:21:22 -0300

    fun

--- git_push.sh
+++ git_push.sh
@@ -4,6 +4,5 @@ commit='user'
 echo "Escreva seu commit"
 read commit 
 echo "Commit: $commit gerado!"
-git commit -m "$commit"
-git push && Tharkminos && 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
-
+git commit -m "$commit" || exit 1
+git push https://Tharkminos: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@github.com/Tharkminos/labdopascal
