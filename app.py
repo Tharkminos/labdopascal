c72ed33a58b6da259c6de790ca077f6ef6a0331b
Tharkminos <guilhermemkii@gmail.com>
2026-07-20 10:40:25 -0300

    B1N1

--- app.py
+++ app.py
@@ -99,8 +99,13 @@ def ler_metadados(md):
                 if ":" in linha:
 
                     chave, valor = linha.split(":", 1)
-
-                    dados[chave.strip()] = valor.strip()
+                    if (valor.strip().lower() == 'false'):
+                        valor = False
+                    elif(valor.strip().lower() == 'true')
+                        valor = True
+                    else:
+                        valor = valor.strip()                    
+                    dados[chave.strip()] = valor
 
     return dados, md
 def obter_xp_total(usuario_id):
