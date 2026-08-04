458d1270eeccfc948f5840f847cc7a162e0048c4
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-08-04 14:45:54 -0300

    Refactor script generation for canvas rendering

--- app.py
+++ app.py
@@ -256,20 +256,20 @@ def processar_etapa(conteudo, banco=None):
     
         if argumentos:
             chamada = f"""<div id="canvas-{sim}"></div>
-    <script>
-    {sim}(
-        document.getElementById("canvas-{sim}"),
-        {{ {argumentos} }}
-    );
-    </script>"""
-        else:
-            chamada = f"""<div id="canvas-{sim}"></div>
-    <script>
-    {sim}(
-        document.getElementById("canvas-{sim}"),
-        {{}}
-    );
-    </script>"""
+<script>
+{sim}(
+    document.getElementById("canvas-{sim}"),
+    {{ {argumentos} }}
+);
+</script>"""
+    else:
+        chamada = f"""<div id="canvas-{sim}"></div>
+<script>
+{sim}(
+    document.getElementById("canvas-{sim}"),
+    {{}}
+);
+</script>"""
     
         return chamada
     
