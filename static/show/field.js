412e75e532c79e3b9786e40673cb5f4bc7171956
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-08-04 15:25:42 -0300

    Change canvas parent to use canvasContainer

--- static/show/field.js
+++ static/show/field.js
@@ -29,7 +29,7 @@ sim.simulacaoConcluida = function(){
 sim.setup = function(){
 
     sim.canvas = sim.createCanvas(370,300);
-    sim.canvas.parent("canvas-field");
+    sim.canvas.parent(sim.canvasContainer);
 
     fieldMaker();
     first();
