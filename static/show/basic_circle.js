f8a86ce409825969dd352a652920557bf16db5ce
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-09-21 12:23:11 -0300

    Update basic_circle.js

--- static/show/basic_circle.js
+++ static/show/basic_circle.js
@@ -1,13 +1,13 @@
 function basic_circle(sim){
-let x = 0;
-let y = 0;
+let x = 200;
+let y = 200;
 let string = "";
 sim.setup = function(){
-sim.canvas = sim.createCanvas(400,500)
+sim.canvas = sim.createCanvas(400,450)
 }
 sim.draw = function(){
 sim.background(220);
-sim.fill(0);
+sim.fill(200,150,80);
 if (sim.mouseIsPressed === true) {
     x = sim.mouseX
     y = sim.mouseY
@@ -17,9 +17,9 @@ sim.fill(255);
 sim.noStroke();
 sim.rect(0,0,400,100)
 sim.stroke(0);
-    sim.fill(0);
+sim.fill(0);
 sim.textSize(18)
-string = "circle("+Math.floor(x)+","+Math.floor(y-100)+",50)"
+string = "circle("+Math.floor(x)+","+Math.floor(y-50)+",50)"
 sim.text(string,100,20)
 }
 }
