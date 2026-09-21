9d28b0a69f433be83547cb16e0c5ecca15574eaf
Tharkminos <guilhermemkii@gmail.com>
2026-09-21 10:19:46 -0300

    PANNA

--- static/show/basic_circle.js
+++ static/show/basic_circle.js
@@ -12,7 +12,7 @@ y = sim.mouseY
 sim.background(220);
 sim.circle(x,y,50)
 sim.textSize(18)
-string = "circle("+Math.floor(x)+","+Math.floor(y)+50+",50)"
+string = "circle("+Math.floor(x)+","+Math.floor(y+50)+",50)"
 sim.text(string,100,20)
 }
 }
