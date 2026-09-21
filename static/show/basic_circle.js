2ffec90d83c37d051fb6108049a033fa3d9077c9
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-09-21 12:13:21 -0300

    Update circle text position in basic_circle.js

--- static/show/basic_circle.js
+++ static/show/basic_circle.js
@@ -11,8 +11,11 @@ x = sim.mouseX
 y = sim.mouseY
 sim.background(220);
 sim.circle(x,y,50)
+sim.fill(255);
+sim.rect(0,0,400,100)
+sim.fill(0);
 sim.textSize(18)
-string = "circle("+Math.floor(x)+","+Math.floor(y+50)+",50)"
+string = "circle("+Math.floor(x)+","+Math.floor(y-100)+",50)"
 sim.text(string,100,20)
 }
 }
