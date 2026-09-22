81590e82d79496e336cf743ed544d350ef6cccbb
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-09-22 14:29:24 -0300

    Fix circle position calculation in basic_circle.js

--- static/show/basic_circle.js
+++ static/show/basic_circle.js
@@ -23,7 +23,6 @@ if(y<5){
     ad_y = 5-Math.floor(sim.mouseY)%5
 }
 
-ad_y = 5-Math.floor(y)%5
 sim.background(220);
 sim.fill(200,150,80);
 
@@ -35,7 +34,7 @@ sim.stroke(0);
 sim.fill(0);
 sim.textSize(18)
 
-string = "circle("+Math.floor(x+ad_x)+","+Math.floor(y-50+ad_y)+",50)"
+string = "circle("+Math.floor(x+ad_x-5)+","+Math.floor(y-50+ad_y-5)+",50)"
 sim.text(string,100,20)
 }
 }
