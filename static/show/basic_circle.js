e60de0d1269c9a1567e47956c1d6b321f5c4a0a6
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-09-22 14:23:11 -0300

    Update mouse position constraints in basic_circle.js
    Adjust mouse position constraints for circle drawing.

--- static/show/basic_circle.js
+++ static/show/basic_circle.js
@@ -9,11 +9,13 @@ sim.canvas = sim.createCanvas(400,450)
 }
 sim.draw = function(){
 if (sim.mouseIsPressed === true) {
-    x = sim.constrain(sim.mouseX+ad_x,0,400)
-    y = sim.constrain(sim.mouseY+ad_y,0,400)
+    x = sim.constrain(sim.mouseX,0,400)
+    y = sim.constrain(sim.mouseY,50,450)
   } 
 ad_x = 5-Math.floor(x)%5
 ad_y = 5-Math.floor(y)%5
+x += ad_x
+y += ad_y
 sim.background(220);
 sim.fill(200,150,80);
 
