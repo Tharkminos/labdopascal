b04265a34c139dfc86721df1a71215d2f4dd6ebe
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-09-22 14:30:05 -0300

    Simplify mouse position adjustment logic
    Refactored the drawing logic to reduce redundancy.

--- static/show/basic_circle.js
+++ static/show/basic_circle.js
@@ -11,17 +11,18 @@ sim.draw = function(){
 if (sim.mouseIsPressed === true) {
     x = sim.constrain(sim.mouseX,0,400)
     y = sim.constrain(sim.mouseY,50,450)
+    if(x<5){
+        x = 0
+        }else{
+        ad_x = 5-Math.floor(sim.mouseX)%5
+        }
+        if(y<5){
+        y = 0
+        }else{
+        ad_y = 5-Math.floor(sim.mouseY)%5
+        }
   } 
-if(x<5){
-    x = 0
-}else{
-    ad_x = 5-Math.floor(sim.mouseX)%5
-}
-if(y<5){
-    y = 0
-}else{
-    ad_y = 5-Math.floor(sim.mouseY)%5
-}
+
 
 sim.background(220);
 sim.fill(200,150,80);
