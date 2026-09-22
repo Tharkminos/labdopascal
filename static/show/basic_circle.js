d088559db446d333a229f8b4079856058d1520d8
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-09-22 14:12:27 -0300

    Adjust circle position based on mouse input with offsets

--- static/show/basic_circle.js
+++ static/show/basic_circle.js
@@ -1,16 +1,20 @@
 function basic_circle(sim){
 let x = 200;
 let y = 200;
+let ad_x = 0;
+let ad_y = 0;
 let string = "";
 sim.setup = function(){
 sim.canvas = sim.createCanvas(400,450)
 }
 sim.draw = function(){
+ad_x = Math.floor(x)%5
+ad_y = Math.floor(y)%5
 sim.background(220);
 sim.fill(200,150,80);
 if (sim.mouseIsPressed === true) {
-    x = sim.mouseX
-    y = sim.mouseY
+    x = sim.mouseX+ad_x
+    y = sim.mouseY+ad_y
   } 
 sim.circle(x,y,50)
 sim.fill(255);
@@ -19,7 +23,8 @@ sim.rect(0,0,400,100)
 sim.stroke(0);
 sim.fill(0);
 sim.textSize(18)
-string = "circle("+Math.floor(x)+","+Math.floor(y-50)+",50)"
+
+string = "circle("+Math.floor(x+ad_x)+","+Math.floor(y-50+ad_y)+",50)"
 sim.text(string,100,20)
 }
 }
