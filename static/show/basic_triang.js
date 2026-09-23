aad1b51c212ca1a2f9a135e7fc00982e42233d4a
Tharkminos <94186863+Tharkminos@users.noreply.github.com>
2026-09-23 11:28:37 -0300

    Refactor basic_triang for better mouse interaction
    Updated triangle drawing logic and adjusted mouse interaction thresholds.

--- static/show/basic_triang.js
+++ static/show/basic_triang.js
@@ -1,10 +1,10 @@
 function basic_triang(sim){
 let x1 = 150;
-let y1 = 100;
+let y1 = 100+50;
 let x2 = 250;
-let y2 = 100;
+let y2 = 100+50;
 let x3 = 200;
-let y3 =  50;
+let y3 =  50+50;
 
 let p1 = 100
 let p2 = 100
@@ -20,17 +20,17 @@ p1 = sim.dist(x1,y1,sim.mouseX,sim.mouseY);
 p2 = sim.dist(x2,y2,sim.mouseX,sim.mouseY);
 p3 = sim.dist(x3,y3,sim.mouseX,sim.mouseY);
 if(sim.mouseIsPressed===true){
-  if(p1<20){
-    x1 = sim.mouseX +5-Math.floor(sim.mouseX)%5
-    y1 = sim.mouseY +5-Math.floor(sim.mouseY)%5
+  if(p1<15){
+    x1 = Math.floor(sim.mouseX +5-Math.floor(sim.mouseX)%5)
+    y1 = sim.mouseY +5-Math.floor(sim.mouseY)%5)
   }
-  if(p2<20){
-    x2 = sim.mouseX +5-Math.floor(sim.mouseX)%5
-    y2 = sim.mouseY +5-Math.floor(sim.mouseY)%5
+  if(p2<15){
+    x2 = Math.floor(sim.mouseX +5-Math.floor(sim.mouseX)%5)
+    y2 = Math.floor(sim.mouseY +5-Math.floor(sim.mouseY)%5)
   }
-  if(p3<20){
-    x3 = sim.mouseX +5-Math.floor(sim.mouseX)%5
-    y3 = sim.mouseY +5-Math.floor(sim.mouseY)%5
+  if(p3<15){
+    x3 = Math.floor(sim.mouseX +5-Math.floor(sim.mouseX)%5)
+    y3 = Math.floor(sim.mouseY +5-Math.floor(sim.mouseY)%5)
   }
 }
 sim.fill(255);
@@ -40,12 +40,12 @@ sim.stroke(0);
 sim.fill(0);
 sim.textSize(18)
 sim.fill(200,0,0);
-sim.circle(x1,y1,20);
-sim.circle(x2,y2,20);
-sim.circle(x3,y3,20);
+sim.circle(x1,y1,10);
+sim.circle(x2,y2,10);
+sim.circle(x3,y3,10);
 sim.fill(0,200,0);
 sim.triangle(x1,y1,x2,y2,x3,y3)
-string = "triangle("+x1+","+y1+","+x2+","+y2+","+x3+","+x3+")"
+string = "triangle("+x1+","+y1+","+x2+","+y2+","+x3+","+y3+")"
 
 sim.fill(0);
 sim.text(string,50,20)
